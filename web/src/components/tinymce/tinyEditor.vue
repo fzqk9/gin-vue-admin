7ec9078e1489ee6ec8c8d98ed64b3eefdd1e8727
88actadmin <10512203@qq.com>
2021-10-11 16:52:28 +0800

    本地化 tinymce

--- web/src/components/tinymce/tinyEditor.vue
+++ web/src/components/tinymce/tinyEditor.vue
@@ -1,158 +1,208 @@
 <template>
- <editor v-model="myTxt"  api-key="API_KEY"  :init="editSetting" />
-	 <template  v-if="beEdit">
-		 <MediaLib ref="mediaLib" @select-one-img="selectOneImg" />
-	 </template>
- </template>
+    <div>
+        <div :class="{fullscreen:fullscreen}" class="tinymce-container" style="width:1000px">
+            <textarea :id="tinymceId" v-model="content" class="tinymce-textarea" />
+        </div>
+    </div>
+</template>
 
- <script>
- 	import {
- 		ref
- 	} from 'vue'
- 	import {
- 		mapGetters
- 	} from 'vuex'
- 	import MediaLib from '@/components/mediaLib/mediaLib.vue'
-	import { uploadFile } from '@/api/common_file' 
-	 import { isEmpty } from '@/utils/utils'
-	import tinymce from '@tinymce/tinymce-vue'  
- 	// const path = import.meta.env.VITE_BASE_PATH+":"+ import.meta.env.VITE_SERVER_PORT "/"
- 	const path = import.meta.env.VITE_BASE_API
- 	export default {
- 		name: 'tinyEditor',
- 		components: {
- 			MediaLib,
-			editor: tinymce
- 		},
- 		props: {
- 			txt: {
- 				type: String,
- 				required: false,
- 				default: 'image'
- 			} 
- 		},
- 		data() {
- 			return { 
- 				myTxt: "",
-				editSetting: {
-				       language: "zh_CN", //语言设置
-				       height: 350, //高度
-				       menubar: false, // 显示最上方menu菜单
-				       toolbar: true, //false禁用工具栏（隐藏工具栏）
-				       browser_spellcheck: true, // 拼写检查
-				       branding: false, // 去水印
-				       statusbar: false, // 隐藏编辑器底部的状态栏
-				       elementpath: false, //禁用下角的当前标签路径
-				       paste_data_images: true, // 允许粘贴图像  
-				       plugin_preview_width:375, // 预览宽度 plugin_preview_height: 668,
-				       plugin_preview_height:600,  
-				       //theme_advanced_buttons3_add : "preview",                            
-				       toolbar: "CardBtn undo redo  | formatselect alignleft aligncenter alignright alignjustify  indent outdent , \
-				            | link unlink | numlist bullist | image media table codesample | fontselect fontsizeselect forecolor backcolor | bold italic underline strikethrough | superscript subscript | removeformat |help code fullscreen preview",
-				       toolbar_drawer: "sliding",
-				       quickbars_selection_toolbar: "removeformat | bold italic underline strikethrough | fontsizeselect forecolor backcolor",
-				       plugins: ['preview link image media table lists fullscreen quickbars', 
-				                'insertdatetime paste code help wordcount codesample'],
-				       content_style: 'img {max-width:100% !important }',         
-				        // init_instance_callback: editor => {
-				        //     if (this.content) {
-				        //         editor.setContent(this.content)
-				        //     }
-				        //     this.finishInit = true
-				        //     editor.on('NodeChange Change SetContent KeyUp', () => {
-				        //         this.hasChanged = true
-				        //     })
-				        // },
-				         // 上传图片
-				        //images_upload_url:"http://localhost:8081/api/commFile/upload",
-						setup: function(editor) { 
-							
-							// editor.PluginManager.add('imageSelector', function(editor, url) {
-							//     // Add a button that opens a window
-							//     editor.addButton('imageSelector', {
-							//         icon: 'image',
-							//         tooltip:"select image from image lib",
-							//         onclick: function() {
-							//             editor.settings.imageSelectorCallback(function(r){
-							//                 console.log('inserting image to editor: '+ r);
-							//                 editor.execCommand('mceInsertContent', false, '<img alt="Smiley face" height="42" width="42" src="' + r + '"/>');
-							//             });
-							//         }
-							//     });
-							
-							// });
-							
-						  // // 注册一个icon
-						  editor.ui.registry.addIcon(
-						    "shopping-cart",
-						    `<svg viewBox="0 0 1024 1024" data-icon="shopping-cart" width="1.5em" height="1.5em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 0 0-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 1 0 0 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 0 0-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 0 0-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 0 0-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 0 0-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 0 1-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 0 1-31.6 31.6z"></path></svg>`
-						  );
-						  // // 注册一个自定义的按钮
-						  editor.ui.registry.addButton("CardBtn", {
-						    icon: `shopping-cart`,
-						    onAction: function(_) {
-								console.log("11111111");
-								//console.log('inserting image to editor: '+ r);
-							     editor.execCommand('mceInsertContent', false, '<img alt="Smiley face" height="42" width="42" src="https://www.88act.com/static/cms/img/index/logo.png"/>');
-						     // that.isShowCard = true;
-						     // that.editor = editor;
-						    }
-						  });
-						},			  
-				        images_upload_handler: (blobInfo, success, failure) => {
-				           let formData = new FormData()
-				           let file =  blobInfo.blob(); 
-				           formData.append('file',file)
-				          // console.log(formData.file)
-				            // let formData = new Object();
-				            
-				             formData.append('name',file.name)
-				             formData.append('size',file.size)
-				             formData.append('md5',"11111")
-				            // formData.size  = file.size;
-				            // formData.type  = file.type;
-				            // formData.file = file.File; 
-				            //console.log(file.File);
-				            uploadFile(formData).then(res => {
-				                console.log(res)
-				                if (res.code == 0) {
-				                    let file2 = res.data;
-				                    success(file2.path);
-				                    return
-				                }
-				                failure('上传失败')
-				            }).catch(() => {
-				                failure('上传出错')
-				            })
-				        } 
-				     }
-				}
- 			}
- 		},
-		async created() {		   
-		    this.myTxt = this.txt; 
-		},
-		watch: {
-		    txt (val) {  
-		     this.myTxt = this.txt;  
-			 console.log("this.myTxt");  console.log(this.myTxt); 
-		    }
-		  }, 
- 		methods: {
- 			openChooseImg() {
- 				console.log("1111");
- 				this.$refs.mediaLib.open()
- 			},
- 			selectOneImg(obj) {
- 				console.log("selectOneImg");
-				console.log(obj);
-				 this.myUrl = obj.url;
-				 this.myList =[obj.url];
-				 this.myGuid = obj.guid;
- 			} 
- 		} 
-		 
-		
- 	}
- </script>
- 
+<script>
+    const tinymceCDN = window.location.origin + '/tinymce/tinymce.min.js'
+    // const tinymceCDN = 'https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js'
+    // import plugins from './plugins.js'
+    // import toolbar from './toolbar.js'
+    import load from './dynamicLoadScript.js'
+	import { uploadFile } from '@/api/common_file'  
+    export default {
+        name: "Tinymce",
+        props : {
+            value: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                hasChange: false,
+                hasInit: false,
+                tinymceId : 'vue-tinymce-' + +new Date() + ((Math.random() * 1000).toFixed(0) + ''),
+                fullscreen: false,
+                toolbar : [],
+                content : ''
+            }
+        },
+       // async created() {
+       //      this.init();
+       //  },
+	   async mounted() {
+	       this.init()
+	   },
+        watch: {
+            value(val) {
+				this.content =val;
+				this.setContent(val);
+				//console.log("watch==========",val);  				
+                // if (!this.hasChange && this.hasInit) {
+                //     this.$nextTick(() =>
+                //     window.tinymce.get(this.tinymceId).setContent(val || ''))
+                // }
+            }
+        },
+        methods: {
+            init() {
+                // dynamic load tinymce from cdn
+                load(tinymceCDN, (err) => {
+                    if (err) {
+                        console.error(err.message)
+                        return
+                    }
+                    window.tinymce.baseURL = window.location.origin + '/tinymce'
+                    this.initTinymce()
+                })
+            },
+            initTinymce() {
+                const _this = this
+                window.tinymce.init({
+					menubar: false, // 显示最上方menu菜单
+                    language: 'zh_CN',
+                    selector: `#${this.tinymceId}`,
+                    height: 350, //高度
+					width: 1300, //高度
+                    body_class: 'panel-body',
+                    object_resizing: false,
+                   // toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,   	
+                   // plugins: plugins,
+                    end_container_on_empty_block: true,
+                    powerpaste_word_import: 'clean',
+                   // code_dialog_height: 450,
+                   // code_dialog_width: 1000,
+                    advlist_bullet_styles: 'square',
+                    advlist_number_styles: 'default',
+                    default_link_target: '_blank',
+                    link_title: false,
+                    nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
+				  branding: false, // 去水印
+				  statusbar: false, // 隐藏编辑器底部的状态栏
+				  elementpath: false, //禁用下角的当前标签路径
+				  paste_data_images: true, // 允许粘贴图像  
+				  //plugin_preview_width:375, // 预览宽度 plugin_preview_height: 668,
+				  //plugin_preview_height:600,  
+					  
+					  toolbar: "undo redo  | formatselect alignleft aligncenter alignright alignjustify  indent outdent , \
+					       | link unlink | numlist bullist |ImageLib image media table codesample | fontselect fontsizeselect forecolor backcolor | bold italic underline strikethrough | superscript subscript | removeformat |help code fullscreen preview",
+					  toolbar_drawer: "sliding",
+					  quickbars_selection_toolbar: "removeformat | bold italic underline strikethrough | fontsizeselect forecolor backcolor",
+					  plugins: ['preview link image media table lists fullscreen quickbars', 
+					           'insertdatetime paste code help wordcount codesample'],
+					  content_style: 'img {max-width:100% !important }',
+				   
+					 init_instance_callback: editor => {
+						if (_this.value) {
+							editor.setContent(_this.value)
+						}
+						_this.hasInit = true
+						editor.on('NodeChange Change KeyUp SetContent', () => {
+							this.hasChange = true
+							this.$emit('input', editor.getContent())
+						})
+                    },
+                    setup(editor) {
+                        editor.on('FullscreenStateChanged', (e) => {
+                            _this.fullscreen = e.state
+                        });
+						// // 注册一个icon
+						editor.ui.registry.addIcon(
+						  "shopping-cart",
+						  `<svg viewBox="0 0 1024 1024" data-icon="shopping-cart" width="1.5em" height="1.5em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 0 0-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 1 0 0 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 0 0-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 0 0-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 0 0-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 0 0-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 0 1-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 0 1-31.6 31.6z"></path></svg>`
+						);
+						// // 注册一个自定义的按钮
+						editor.ui.registry.addButton("ImageLib", {
+						  icon: `shopping-cart`,
+						  onAction: function(_) {
+						 console.log("11111111");
+						 //console.log('inserting image to editor: '+ r);
+						  editor.execCommand('mceInsertContent', false, '<img alt="Smiley face" height="42" width="42" src="https://www.88act.com/static/cms/img/index/logo.png"/>');
+					   // that.isShowCard = true;
+						   // that.editor = editor;
+						  }
+						});
+                    },
+					images_upload_handler: (blobInfo, success, failure) => {
+					   let formData = new FormData()
+					   let file =  blobInfo.blob(); 
+					   formData.append('file',file)
+					  // console.log(formData.file)
+					    // let formData = new Object();
+					    
+					     formData.append('name',file.name)
+					     formData.append('size',file.size)
+					     formData.append('md5',"11111")
+					    // formData.size  = file.size;
+					    // formData.type  = file.type;
+					    // formData.file = file.File; 
+					    //console.log(file.File);
+					    uploadFile(formData).then(res => {
+					        console.log(res)
+					        if (res.code == 0) {
+					            let file2 = res.data;
+					            success(file2.path);
+					            return
+					        }
+					        failure('上传失败')
+					    }).catch(() => {
+					        failure('上传出错')
+					    })
+					} 
+                })
+            },
+			
+	/*		
+			language: "zh_CN", //语言设置
+			height: 350, //高度
+			menubar: false, // 显示最上方menu菜单
+			toolbar: true, //false禁用工具栏（隐藏工具栏）
+			browser_spellcheck: true, // 拼写检查
+			branding: false, // 去水印
+			statusbar: false, // 隐藏编辑器底部的状态栏
+			elementpath: false, //禁用下角的当前标签路径
+			paste_data_images: true, // 允许粘贴图像  
+			plugin_preview_width:375, // 预览宽度 plugin_preview_height: 668,
+			plugin_preview_height:600,  
+			//theme_advanced_buttons3_add : "preview",                            
+			toolbar: "undo redo  | formatselect alignleft aligncenter alignright alignjustify  indent outdent , \
+			     | link unlink | numlist bullist | image media table codesample | fontselect fontsizeselect forecolor backcolor | bold italic underline strikethrough | superscript subscript | removeformat |help code fullscreen preview",
+			toolbar_drawer: "sliding",
+			quickbars_selection_toolbar: "removeformat | bold italic underline strikethrough | fontsizeselect forecolor backcolor",
+			plugins: ['preview link image media table lists fullscreen quickbars', 
+			         'insertdatetime paste code help wordcount codesample'],
+			content_style: 'img {max-width:100% !important }',  
+		*/	
+			
+            setContent(value) {
+                window.tinymce.get(this.tinymceId).setContent(value)
+            },
+            getContent() {
+                window.tinymce.get(this.tinymceId).getContent()
+            },
+            destroyTinymce() {
+                const tinymce = window.tinymce.get(this.tinymceId)
+
+                if (tinymce) {
+                    tinymce.destroy()
+                }
+            },
+        },
+    
+        activated() {
+            if (window.tinymce) {
+                this.initTinymce()
+            }
+        },
+        deactivated() {
+            this.destroyTinymce()
+        },
+        destroyed() {
+            this.destroyTinymce()
+        }
+    }
+</script>
